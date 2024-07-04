b32b8fc7f3c3276c10c97677db42b51bde5295f6
Ridvan <ridvans@abv.bg>
2024-07-04 17:58:26 +0300

    updating

--- JavaExamples/lib/src/main/java/com/example/lib/MyClass.java
+++ JavaExamples/lib/src/main/java/com/example/lib/MyClass.java
@@ -1,4 +1,25 @@
 package com.example.lib;
 
 public class MyClass {
+
+    public static void main(String[] args){
+
+
+
+    }
+}
+class car{
+    int year;
+    int speed;
+
+    void accelerate(){
+        speed += 10;
+    }
+    void breaking(){
+        speed -= 5;
+    }
+    String showSpeed(){
+        return "The speed is " +speed;
+    }
+
 }
--- JavaExamples/settings.gradle.kts
+++ JavaExamples/settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "JavaExamples"
 include(":app")
- 
+include(":lib")
