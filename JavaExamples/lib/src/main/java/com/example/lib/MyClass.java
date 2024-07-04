a224b982e955e824a53d18d52a17db7347eb67e8
Ridvan <ridvans@abv.bg>
2024-07-04 18:07:07 +0300

    object

--- JavaExamples/lib/src/main/java/com/example/lib/MyClass.java
+++ JavaExamples/lib/src/main/java/com/example/lib/MyClass.java
@@ -4,18 +4,25 @@ public class MyClass {
 
     public static void main(String[] args){
 
+        //className object = new className();
+        Car redCar = new Car();
+        redCar.speed = 150;
+        redCar.year = 2020;
+        redCar.accelerate();
+        redCar.brake();
+        String speedTxt = redCar.showSpeed();
 
-
+        System.out.println(speedTxt);
     }
 }
-class car{
+class Car{
     int year;
     int speed;
 
     void accelerate(){
         speed += 10;
     }
-    void breaking(){
+    void brake(){
         speed -= 5;
     }
     String showSpeed(){
